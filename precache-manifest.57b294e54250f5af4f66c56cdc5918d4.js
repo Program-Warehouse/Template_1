17808a591e8fdefd71bf70429aea3b4430120d07
Lakshya Tyagi <tyagicoolboy22@gmail.com>
2020-12-17 00:22:41 +0530

    Add files via upload

---
+++ precache-manifest.57b294e54250f5af4f66c56cdc5918d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9af33804cdc465f56a5c10675905b30c",
+    "url": "/Template_1/index.html"
+  },
+  {
+    "revision": "4cf954af86fe641d5a25",
+    "url": "/Template_1/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "4503ce833e42471a34dc",
+    "url": "/Template_1/static/js/2.e3d68ef1.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/Template_1/static/js/2.e3d68ef1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cf954af86fe641d5a25",
+    "url": "/Template_1/static/js/main.35f72e8c.chunk.js"
+  },
+  {
+    "revision": "6e5440395af5c1f08374",
+    "url": "/Template_1/static/js/runtime-main.8504f68f.js"
+  },
+  {
+    "revision": "4bf1de5e65f728ec73a66e7715f12854",
+    "url": "/Template_1/static/media/resume.4bf1de5e.pdf"
+  }
+]);
